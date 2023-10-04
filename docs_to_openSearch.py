fc391f957800b87a9d1994e41ae0a8b1a1dc827e
Ryan Doty <rdoty@amazon.com>
2023-10-04 08:31:14 -0400

    package upgrades

--- docs_to_openSearch.py
+++ docs_to_openSearch.py
@@ -11,7 +11,7 @@ load_dotenv()
 
 # instantiating the bedrock client, with specific CLI profile
 boto3.setup_default_session(profile_name=os.getenv('profile_name'))
-bedrock = boto3.client('bedrock', 'us-east-1', endpoint_url='https://bedrock.us-east-1.amazonaws.com')
+bedrock = boto3.client('bedrock-runtime', 'us-east-1', endpoint_url='https://bedrock.us-east-1.amazonaws.com')
 opensearch = boto3.client("opensearchserverless")
 
 # Instantiating the OpenSearch client, with specific CLI profile
@@ -31,6 +31,7 @@ client = OpenSearch(
 )
 
 # loading in PDF, can use PyPDFDirectoryLoader if you want to load in a directory of PDFs
+# TODO: Change PDF loader and chunker
 loader = PyPDFLoader("path/to/PDF")
 documents = loader.load()
 
--- query_against_openSearch.py
+++ query_against_openSearch.py
@@ -9,7 +9,7 @@ load_dotenv()
 
 # instantiating the Bedrock client, and passing in the CLI profile
 boto3.setup_default_session(profile_name=os.getenv('profile_name'))
-bedrock = boto3.client('bedrock', 'us-east-1', endpoint_url='https://bedrock.us-east-1.amazonaws.com')
+bedrock = boto3.client('bedrock-runtime', 'us-east-1', endpoint_url='https://bedrock.us-east-1.amazonaws.com')
 
 # instantiating the OpenSearch client, and passing in the CLI profile
 opensearch = boto3.client("opensearchserverless")
